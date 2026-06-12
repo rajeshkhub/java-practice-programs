3d893dedeae9b2f39058b2e3a735e297564a4d68
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-12 10:14:22 +0530

    Refactor ReverseString class for clarity

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -6,12 +6,14 @@ public class ReverseString {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a string : ");
 		String word = sc.nextLine();
-		StringBuilder sb = new StringBuilder(word);
+		StringBuilder sb = new StringBuilder(word); //StringBuilder
 		System.out.print("Now your string is in reverse gear : ");
+		
 		//or using for loop method
 		/*for(int i=word.length()-1;i>=0;i--) {
 			System.out.print(word.charAt(i));
 		}*/
+		
 		System.out.println(sb.reverse()); //stringbuilder method
 	}
 }
