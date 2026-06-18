aa52a3f9ead710aedced4f60ca9863c0c136a729
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-18 23:47:41 +0530

    Update and rename basics/OddrEven.java to basics/Odd/Even.java

--- basics/Odd/Even.java
+++ basics/Odd/Even.java
@@ -1,25 +1,24 @@
-package ippo;
 import java.util.*;
-public class OddrEven {
+public class OddEven {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 while(true){try{
 System.out.println("Enter a number : ");
-int a = sc.nextInt();
-String c = (a%2==0)? "Even" : "Odd";
-System.out.println("The given number "+a+" is : "+c);}
+int num = sc.nextInt();
+String word = (num % 2 == 0)? "Even" : "Odd";
+System.out.println("The given number "+num+" is : "+word);}
 catch(InputMismatchException e){
 	System.out.println("Invalid input!");
 	sc.next();
 	continue;}
-String b;
+String word2;
 while(true) {
 System.out.println("Do u want to continue : yes/no");
-b=sc.next().toLowerCase();
-if(b.equals("yes")) break;
-else if(b.equals("no")) break;
+word2 = sc.next().toLowerCase();
+if(word2.equals("yes")) break;
+else if(word2.equals("no")) break;
 else {System.out.println("Invalid answer.");continue;}}
-if(b.equals("no")) break;}
+if(word2.equals("no")) break;}
 System.out.println("Thank you");
 sc.close();}}
