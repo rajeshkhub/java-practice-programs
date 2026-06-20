90a905f60db55502b752ac2ed5f0fb38149e1359
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-20 23:53:08 +0530

    Refactor PrimeNum class for clarity and structure
    Refactored prime number checking logic and improved code readability.

--- basics/PrimeNum.java
+++ basics/PrimeNum.java
@@ -1,24 +1,28 @@
 import java.util.*;
 public class PrimeNum {
-
 	public static void main(String[] args) {
-		
+		//Checks whether the number is prime number or not 
 Scanner sc = new Scanner(System.in);
 while(true) {try {
 	System.out.println("Enter a number : ");
 	int num = sc.nextInt();
-	if(num<=1) {
-		System.out.println("Not a prime number.");}
+	if(num <= 1) {
+		System.out.println("Not a prime number.");
+	}
 	else{ boolean prime = true;
-		for(int i =2;i<=Math.sqrt(num);i++) {
-			if(num%i==0) { prime = false;
-			break;}}
+		for(int i = 2;i <= Math.sqrt(num);i++) {
+			if(num % i == 0) { prime = false;
+			break;}
+		}
 		if(prime) {System.out.println("The given numer "+num+" is :\n A Prime number");}
-		else {System.out.println("The given number "+num+" is :\n Not a Prime number");}}
+		else {System.out.println("The given number "+num+" is :\n Not a Prime number");}
+		}
 	break;}
 catch(InputMismatchException e) {
 	System.out.println("Invalid input!");
-} sc.next();}
+} 
+sc.next();
+}
 sc.close();
 }
-	}
+}
