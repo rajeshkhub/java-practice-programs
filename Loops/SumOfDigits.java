55bfc0255f7ec250f3929e29c651437d66c140d9
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-21 00:16:16 +0530

    Add comment to clarify sum of digits functionality

--- Loops/SumOfDigits.java
+++ Loops/SumOfDigits.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class SumOfDigits {
-
 	public static void main(String[] args) {
-		
+		// Gives output of Sum of all digits in a number.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number : ");
 		int num = sc.nextInt();
