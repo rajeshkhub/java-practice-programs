685f6c989da20e361a30c4e80e5390681985e97f
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-22 15:36:48 +0530

    Refactor VowelCount.java for clarity and readability
    Refactored VowelCount.java to improve readability and added a comment for clarity.

--- Strings/VowelCount.java
+++ Strings/VowelCount.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 class VowelCount {
 	public static void main(String[] args) {
-		
+		// Counts number of vowels in a string.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter any string/word : ");
 			String word1 = sc.nextLine();
 			String word2 = word1.toLowerCase();
 			int count = 0;
-			char[] a = word2.toCharArray();       //String becomes characters of array
-
+			char[] a = word2.toCharArray();       //String becomes array of characters
 			for(char b : a) {                                               //for each loop
 				if(b=='a' || b=='e' || b=='i' || b=='o' || b=='u') {         
 				c++;
 				}
-				else {}}
-		
+				else {}
+			}
 			System.out.println("Number of vowels in the given string : "+count);
 			sc.close();
 	}
