0d4f5fb10ffbc8e0073608199b7f1d5f1b0e7610
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-22 15:35:14 +0530

    Refactor ReverseString.java for clarity
    Updated comments for clarity and removed unnecessary lines.

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,20 +1,17 @@
 import java.util.*;
 public class ReverseString {
-
 	public static void main(String[] args) {
-		
+		// Returns reversed form of a word by reversing it using String builder / for loop.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a string : ");
 		String word = sc.nextLine();
 		sc.close();
 		StringBuilder sb = new StringBuilder(word); //StringBuilder
 		System.out.print("Now your string is in reverse gear : ");
-		
 		//or using for loop method
 		/*for(int i=word.length()-1;i>=0;i--) {
 			System.out.print(word.charAt(i));
 		}*/
-		
 		System.out.println(sb.reverse()); //stringbuilder method
 	}
 }
