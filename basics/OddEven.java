792adc435e9f8b793f730863f0717a47e1741a7c
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-22 15:53:34 +0530

    Refactor OddEven.java with comments
    Added comments for clarity and improved code readability.

--- basics/OddEven.java
+++ basics/OddEven.java
@@ -1,24 +1,32 @@
 import java.util.*;
 public class OddEven {
 	public static void main(String[] args) {
-		
+		// Finds whether the number is odd or even number.
 		Scanner sc = new Scanner(System.in);
 while(true){try{
 System.out.println("Enter a number : ");
 int num = sc.nextInt();
 String word = (num % 2 == 0)? "Even" : "Odd";
-System.out.println("The given number "+num+" is : "+word);}
+System.out.println("The given number "+num+" is : "+word);
+}
 catch(InputMismatchException e){
 	System.out.println("Invalid input!");
 	sc.next();
-	continue;}
+	continue;
+}
 String word2;
 while(true) {
 System.out.println("Do u want to continue : yes/no");
 word2 = sc.next().toLowerCase();
 if(word2.equals("yes")) break;
 else if(word2.equals("no")) break;
-else {System.out.println("Invalid answer.");continue;}}
+else {
+	System.out.println("Invalid answer.");
+	  continue;
+	 }
+}
 if(word2.equals("no")) break;}
 System.out.println("Thank you");
-sc.close();}}
+sc.close();
+	}
+}
