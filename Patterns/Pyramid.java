af47a84212b01fee1f795ead88058fae0ad52fa2
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-24 19:35:50 +0530

    Rename Patterns class to Pyramid

--- Patterns/Pyramid.java
+++ Patterns/Pyramid.java
@@ -1,18 +1,16 @@
-package testt;
 import java.util.Scanner;
-public class Patterns2 {
-
+public class Pyramid {
 	public static void main(String[] args) {
-		
+		//Pyramid pattern
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Pyramid height : ");
 		int height = sc.nextInt();
 		sc.close();
-		for(int i = 1; i <= height; i++) {
-			for(int j = 1; j <= height-i; j++) {
+		for(int i = 1; i <= height; i++) {             //Row loop
+			for(int j = 1; j <= height-i; j++) {       //Space loop
 				System.out.print(" ");
 			}
-			for(int k = 1; k <= 2*i-1; k++) {
+			for(int k = 1; k <= 2*i-1; k++) {          //Star loop
 				System.out.print("*");
 			}
 			System.out.println();
