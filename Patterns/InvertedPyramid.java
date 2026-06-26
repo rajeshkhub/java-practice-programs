06ab87faadbf4c0e4a3147ed5394a3e5be05c5e6
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-26 16:16:53 +0530

    Create InvertedPyramid.java

---
+++ Patterns/InvertedPyramid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class InvertedPyramid {
+	public static void main(String[] args) {
+//Prints
+Scanner sc = new Scanner(System.in);
+		System.out.println("Enter height :");
+int height = sc.nextInt();
+sc.close();
+		for(int i = 1; i <= height; i++) {
+			for(int j = 1; j < i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = i; j <= 2*height-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
