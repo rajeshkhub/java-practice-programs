67f037440675a084f024084f20fdbdc1a979f7fd
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-26 16:19:29 +0530

    Fix inverted pyramid printing logic

--- Patterns/InvertedPyramid.java
+++ Patterns/InvertedPyramid.java
@@ -7,9 +7,11 @@ Scanner sc = new Scanner(System.in);
 int height = sc.nextInt();
 sc.close();
 		for(int i = 1; i <= height; i++) {
+			//space loop
 			for(int j = 1; j < i; j++) {
 				System.out.print(" ");
 			}
+			//star loop
 			for(int j = i; j <= 2*height-i; j++) {
 				System.out.print("*");
 			}
