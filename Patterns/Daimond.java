311b4f571798d46fb8669ed3a1e506c2b2832189
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-28 15:58:46 +0530

    Add files via upload

---
+++ Patterns/Daimond.java
@@ -0,0 +1,28 @@
+package testt;
+import java.util.Scanner;
+public class Daimond {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter height : ");
+		int height = sc.nextInt();
+		sc.close();
+		int mid = (height+1)/2;
+		
+		for(int i = 1; i <= height; i++) {
+			int level;
+			if(i <= mid) {
+				level = i;
+			}
+			else level = height - (i - 1);
+			for(int j = height-level; j >= mid; j--) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= 2*level-1 ; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			}
+	}
+
+}
