10bce0eb2e1aec4d4c089c66b0777c4612aa7a2c
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-28 16:03:55 +0530

    Add comments to Daimond.java for clarity
    Added comments to clarify the purpose of the code and loops.

--- Patterns/Daimond.java
+++ Patterns/Daimond.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Daimond {
 	public static void main(String[] args) {
-		
+		// Generates Daimond shaped pattern.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter height : ");
 		int height = sc.nextInt();
@@ -14,9 +14,11 @@ public class Daimond {
 				level = i;
 			}
 			else level = height - (i - 1);
+			// Space loop
 			for(int j = height-level; j >= mid; j--) {
 				System.out.print(" ");
 			}
+			// Star loop
 			for(int j = 1; j <= 2*level-1 ; j++) {
 				System.out.print("*");
 			}
