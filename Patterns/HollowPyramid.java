f4ca9e6324bc0d3fe76b1242b47995614f46c071
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-30 22:05:18 +0530

    Update HollowPyramid.java

--- Patterns/HollowPyramid.java
+++ Patterns/HollowPyramid.java
@@ -11,10 +11,12 @@ public class HollowPyramid {
 				System.out.print(" ");
 			}
 			for(int j = 1; j <= 2*i-1; j++) {
-				if(i != 1 && i != height && j != 1 && j != 2*i-1) {
-						System.out.print(" ");
+				if(i == height || j == 1 || j == 2*i-1) {
+						System.out.print("*");
 				}
-				else System.out.print("*");
+				else System.out.print(" ");
 			}
 			System.out.println();
 		}
+	}
+}
