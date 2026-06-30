c371e9445522204238e057690fd9f2060321e1be
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-06-30 22:26:50 +0530

    Fix logic for calculating levels in Butterfly pattern

--- Patterns/Butterfly.java
+++ Patterns/Butterfly.java
@@ -6,11 +6,11 @@ public class Butterfly {
 		System.out.println("Enter height :");
 		int height = sc.nextInt();
 		sc.close();
-    int mid = (height+1)/2;
+		int mid = (height+1)/2;
 		for(int i = 1; i <= height; i++) {
 			int level;
 			if(i <= mid) level = i;
-			else level = height - (i-1);
+			else level = height - i+1;
 			for(int j = 1; j <= level; j++) {
 				System.out.print("*");
 			}
@@ -22,3 +22,5 @@ public class Butterfly {
 			}
 			System.out.println();
 		}
+	}
+}
