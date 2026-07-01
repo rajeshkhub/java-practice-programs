fce7a0733e005b08d3c5d39ce9fff8b58e032743
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-01 21:49:04 +0530

    Add PascalTriangle.java to print Pascal's triangle

---
+++ Patterns/PascalTriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PascalTrangle {
+	public static void main(String[] args) {
+    
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter height :");
+		int height = sc.nextInt();
+		sc.close();
+    int[] prev = null;
+		for(int i = 1; i <= height; i++) {
+			for(int j = 1; j <= height-i; j++) {
+				System.out.print(" ");
+			}
+			int[] current = new int[i];
+			for(int j = 1; j <= i; j++) {
+				if(j == i || j == 1) current[j-1] = 1;
+				else current[j-1] = prev[j-2]+prev[j-1];
+				System.out.print(current[j-1]+" ");
+			}
+			prev = current;
+			System.out.println();
+		}
+  }
+}
