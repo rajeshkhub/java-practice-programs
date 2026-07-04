5d9e157ff3445a38625bd9d9a2729602e976a166
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-04 18:43:42 +0530

    Refactor CountingDigits.java for clarity
    Added a comment to clarify the length calculation of the number.

--- Strings/CountingDigits.java
+++ Strings/CountingDigits.java
@@ -1,17 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
 public class CountingDigits {
 	public static void main(String[] args) {
-		// Counts the number of digits present in a number.
+																// Counts the number of digits present in a number.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your number : ");
 		int num = sc.nextInt();
-		// using string method
+																// using string method
 		/*
 		 * String a = sc.next();
 		 * System.out.println("Number of digits in the number are : "+a.length());
 		 */
 		sc.close();
-		int length = String.valueOf(num).length();
+		int length = String.valueOf(num).length();				//Converts int num variable into string and generates the length of it.
 		System.out.println("No.of digits : "+length);
 	}
 }
