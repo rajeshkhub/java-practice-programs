b6b91b17cf6411d1767ae67dd640be6deb74b6f0
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-04 10:22:36 +0530

    Refactor anagram check implementation
    Refactored anagram check logic to improve readability and maintainability.

--- Strings/AnagramCheck.java
+++ Strings/AnagramCheck.java
@@ -7,23 +7,23 @@ public class AnagramCheck {
 		String word = sc.next().toLowerCase();
 		String word2 = sc.next().toLowerCase();
 		boolean found = false;
-		boolean[] visited = new boolean[word2.length()];
-		if(word.length() == word2.length()) {
-			for(int i = 0; i < word.length(); i++) {
+		boolean[] visited = new boolean[word2.length()]; //Boolean array to store visited or not, in indexes parallel to the second string indexes.
+		if(word.length() == word2.length()) {                          //Checks whether the two strings have equal lenghts.
+			for(int i = 0; i < word.length(); i++) {                   //Iteration over first string.
 				found = false;
-				for(int j = 0; j < word2.length(); j++) {
-					if(!visited[j]) {
-						if(word.charAt(i) == word2.charAt(j)) {
-							found = true;
-							visited[j] = true;
+				for(int j = 0; j < word2.length(); j++) {              //Iteration over second string.
+					if(!visited[j]) {                                  //Checks if the boolean array element at index j is visited or not.
+						if(word.charAt(i) == word2.charAt(j)) {         //Compares character of first string at index i with character of second string with index j.
+							found = true;                              //Assigns true to found if the above condition is true.
+							visited[j] = true;                         // Marks the visited array true at index j.
 							break;
 						}
 					}
 				}
-				if(!found) break;
+				if(!found) break;                                //checks if found is false 
 			}
 		}
-		if(found) System.out.println("Anagram");
-		else System.out.println("Not anagram");
+		String result = (found)? "Anagrams" : "Not Anagrams";
+		System.out.println("The given two strings are "+result);
 	}
 }
