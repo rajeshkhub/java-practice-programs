9075c8f5e9ea172d0d7fa83528babd84c9c4c3a2
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-04 11:08:19 +0530

    Enhance DuplicateCharRemover with explanatory comments
    Added comments to explain the logic for removing duplicate characters.

--- Strings/DuplicateCharRemover.java
+++ Strings/DuplicateCharRemover.java
@@ -3,17 +3,18 @@ public class DuplicateCharRemover {
 	public static void main(String[] args) {
 		// Removes duplicate characters in given string using stringbuilder.
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any word/String :")
 		String word = sc.next();
 		sc.close();
-		StringBuilder sb = new StringBuilder(word);
-		for(int i = 0; i < sb.length(); i++) {
-			for(int j = i+1; j < sb.length(); j++) {
-				if(Character.toLowerCase(sb.charAt(i)) == Character.toLowerCase(sb.charAt(j))) {
-					sb.deleteCharAt(j);
-					j--;
+		StringBuilder sb = new StringBuilder(word);                //Word passed to the StringBuilder constructor as argument.
+		for(int i = 0; i < sb.length(); i++) {                     //i loop starts from 0 index of string word.
+			for(int j = i+1; j < sb.length(); j++) {               //j loop starts from a index before i.
+				if(Character.toLowerCase(sb.charAt(i)) == Character.toLowerCase(sb.charAt(j))) {             //compares the characters of index i and index j in lowercase using Character.toLowercase() method.
+					sb.deleteCharAt(j);                            //Deletes/removes the duplicate characters in the string.
+					j--;                                           //decrements j by 1 so that comparison is performed on every character.
 				}
 			}
 		}
-		System.out.print(sb);
+		System.out.print("String after removing duplicates : "+sb);
 	}
 }
