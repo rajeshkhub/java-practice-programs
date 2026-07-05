6ce4c4fafaad9ffdbca4451d84eeb82e6b06c7ed
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-05 13:55:55 +0530

    Refactor comments in ReverseString.java
    Updated comments for clarity and added explanations for StringBuilder usage.

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,17 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
 public class ReverseString {
 	public static void main(String[] args) {
-		// Returns reversed form of a word by reversing it using String builder / for loop.
+																	// Returns reversed form of a word by reversing it using String builder / for loop.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a string : ");
 		String word = sc.nextLine();
 		sc.close();
-		StringBuilder sb = new StringBuilder(word); //StringBuilder
+		StringBuilder sb = new StringBuilder(word);  				//Assigning string word to StringBuilder constructor.
 		System.out.print("Now your string is in reverse gear : ");
-		//or using for loop method
-		/*for(int i=word.length()-1;i>=0;i--) {
-			System.out.print(word.charAt(i));
+																	//or using for loop method.
+		/*for(int i = word.length()-1; i >= 0; i--) {				//Loop starts from last index of string word.
+			System.out.print(word.charAt(i));						//Prints characters of word from last index.
 		}*/
-		System.out.println(sb.reverse()); //stringbuilder method
+		System.out.println(sb.reverse()); 							//stringbuilder reverse method.
 	}
 }
