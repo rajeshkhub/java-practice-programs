05ddcfaa29e383333e3b7e9b64cbfbfcb3953efc
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-06 19:48:42 +0530

    Refactor LinearSearch with improved variable names
    Updated variable names and added comments for clarity.

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,30 +1,30 @@
 import java.util.*;
 public class LinearSearch {
 	public static void main(String[] args) {
-		// Finds any element index position in a array.
+																						// Finds target element index position in a array.
                Scanner sc = new Scanner(System.in);
                System.out.println("Enter the array size : ");
                int size = sc.nextInt();
-               int array[] = new int[size];
+               int array[] = new int[size];												//Assigning size to the array.
                System.out.println("Enter your array elements : ");
-               for(int i=0;i<size;i++) {
+               for(int i = 0; i < size; i++) {											//For loop to insert input numbers into the array.
 	                   array[i] = sc.nextInt();
                }
                System.out.print("Your array elements are : ");
-               for(int i=0;i<array.length;i++) {
+               for(int i = 0; i < array.length; i++) {									//For loop to print array elements.
                        System.out.print(array[i]+" ");
 			   }
                System.out.println("\nWhich element you want to search : ");
-               int element = sc.nextInt();
-               boolean found = true;
-		       //finding wanted number in the array
-               for(int i=0;i<array.length;i++) {
-	                     if(element == array[i]) {
-		                         System.out.println("Element found at index : "+i);
-                                 found = false;
-                         break;}
+               int target = sc.nextInt();												//target element.
+               boolean found = false;													
+               for(int i = 0; i < array.length; i++) {									//For loop for finding target element in the array.
+	                     if(target == array[i]) {
+		                         System.out.println("Element found at index : "+i);		//prints index of target element in the array.
+                                 found = true;											//found becomes true when target element is found.
+                         break;
+						 }
                }
-   if(found){
+   if(!found){																			//condition if target is not found.
 	   System.out.println("Element not found in the list.");
    }
    sc.close();
