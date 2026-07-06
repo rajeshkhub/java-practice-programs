8eacb930fb919a3e9264e0f82b9f8c8972555776
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-06 21:21:17 +0530

    Enhance MinMax.java with additional comments
    Added comments to clarify code functionality and purpose.

--- Arrays/MinMax.java
+++ Arrays/MinMax.java
@@ -5,18 +5,18 @@ public class MinMax {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the size of array : ");
 		int size = sc.nextInt();
-		int[] array = new int[size];
+		int[] array = new int[size];																//Assigning size to array.
 		System.out.println("Enter your elements.");
-		for(int i = 0; i < size ; i++) {
+		for(int i = 0; i < size ; i++) {															//For loop to insert input elements in array.
 			array[i] = sc.nextInt();
 		}
 		System.out.print("Your array elements are : ");
-		for(int i = 0; i < array.length ;i++) {
+		for(int i = 0; i < array.length ;i++) {														//For loop to print array elements.
 			System.out.print(array[i]+" ");
 		}
-		while(true) {
+		while(true) {																				//While loop (continues loop if user gives invalid input).
 			System.out.println("\nChoose your option : \n1. Maximum element \n2. Minimum element");
-																									//MAX
+																									//Max block
 			int choice = sc.nextInt();
 			if(choice == 1) { int max = array[0];
 			for(int i = 0; i < array.length;i++) {
@@ -25,7 +25,7 @@ public class MinMax {
 				}
 			}
 			System.out.println("\nMax of the array : "+max); break;}
-																									//MIN
+																									//MIN block
 			else if(choice == 2) {int min = array[0];
 			for(int i = 0 ; i < array.length ;i++) {
 				if(array[i] < min) {
