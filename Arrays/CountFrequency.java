53bded63545721753c4975e015106078d59ebf8b
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-06 19:29:09 +0530

    Enhance CountFrequency.java with detailed comments
    Added comments for better code understanding and clarity.

--- Arrays/CountFrequency.java
+++ Arrays/CountFrequency.java
@@ -1,23 +1,23 @@
 import java.util.*;
 public class CountFrequency {
 	public static void main(String[] args) {
-		// Counts the number of occurrence of each element in a array.
+																// Counts the number of occurrence of each element in a array.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the arry size : ");
-		int size=sc.nextInt();
-		int box[] = new int[size];
+		int size = sc.nextInt();
+		int box[] = new int[size];								//Assigning size to the box array.
 		System.out.println("Enter elements :");
-		for(int i=0;i<size;i++) {
-			box[i]=sc.nextInt();
+		for(int i = 0; i < size; i++) {							//For loop to insert input integers into the array.
+			box[i] = sc.nextInt();
 		}
 		sc.close();
-		for(int i=0;i<box.length;i++) {
-			if(box[i]==-1) continue;
-			int count =1;
-			for(int j=i+1;j<box.length;j++) {
-				if(box[i]==box[j]) {
-					count++;
-					box[j]=-1;
+		for(int i = 0; i < box.length; i++) {					//For loop starts from 0 index.
+			if(box[i] == -1) continue;							//Condition to skip elements marked as "-1". 
+			int count = 1;
+			for(int j = i+1; j < box.length; j++) {				//Nested for loop starts from index 1.
+				if(box[i] == box[j]) {
+					count++;									//Count increment if above condition is true.
+					box[j] = -1;								//Replacing visited element with -1.
 				}
 			}
 			System.out.println(box[i]+" occurs "+count+" time/s.");
