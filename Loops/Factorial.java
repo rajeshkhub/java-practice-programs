45eca886b62482d87e24ccb2111ba7ac78b189de
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-07 14:15:08 +0530

    Refactor factorial calculation using for loop
    Refactored factorial calculation to use a for loop instead of a while loop.

--- Loops/Factorial.java
+++ Loops/Factorial.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Factorial {
 	public static void main(String[] args) {
-		// Returns factorial of a number.
+																		// Returns factorial of a number.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number : ");
 		int num = sc.nextInt();
@@ -10,11 +10,18 @@ public class Factorial {
 			System.out.println("The factorial of "+num+" is : 1");
 		}
 		int num1 = 1;
-		int num2 = num;
+																		//Using While loop.
+		/*int num2 = num;
 		while(num > 0) {
 		 num1 *= num;
 		 num--;
 		}
 		System.out.println("The factorial of "+num2+" is : "+num1);
+		*/
+																		//Using For loop.
+		for(int i = num; i > 1; i--) {
+			num1 *= i;
+		}
+		System.out.println("The factorial of "+num+" is : "+num1);
 	}
 }
