5da2e795ea0e2d028b5af3953d5be3936d86c18f
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-07 14:36:25 +0530

    Refactor Fibonacci.java for readability
    Reformatted code and added comments for clarity.

--- Loops/Fibonacci.java
+++ Loops/Fibonacci.java
@@ -1,7 +1,7 @@
-import java.util.*;
+import java.util.Scanner;
 public class Fibonacci {
 	public static void main(String[] args) {
-		// Gives n number of Fibonacci series number.
+																					// Gives n number of Fibonacci series number.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter how many terms :");
 		int terms = sc.nextInt();
@@ -10,11 +10,11 @@ public class Fibonacci {
 		int first = 0;
 		int second = 1;
 		if(terms == 1){
-			System.out.print("The fibonacci sequence : "+first);
+			System.out.print("The fibonacci sequence : "+first);					
 		}
 		else System.out.print("The fibonacci sequence : "+first+" "+second+" ");
-		while(num <= terms-1) {
-			int temp = first+second;
+		while(num <= terms-1) {       												//Using while loop.
+			int temp = first + second;
 			first = second;
 			second = temp;
 			System.out.print(temp+" ");
