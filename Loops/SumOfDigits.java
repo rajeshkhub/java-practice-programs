0311166e896f59f8c37ecf4eae680966bec822c9
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-07 23:53:12 +0530

    Refactor SumOfDigits.java with comments and formatting
    Added comments for clarity and formatting improvements.

--- Loops/SumOfDigits.java
+++ Loops/SumOfDigits.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class SumOfDigits {
 	public static void main(String[] args) {
-		// Gives output of Sum of all digits in a number.
+															// Gives output of Sum of all digits in a number.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number : ");
 		int num = sc.nextInt();
 		int sum = 0;
+															//Using While loop.
 		while(num > 0) {
-			sum += num%10;
+			sum += num % 10;
 			num /= 10;
 		}
 		System.out.println("Sum of digits : "+sum);
