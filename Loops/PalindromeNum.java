0907f37c5474df1d0a541ec1c5a8a058112603c6
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-08 18:01:29 +0530

    Enhance PalindromeNum.java with explanatory comments
    Added comments to clarify the palindrome checking logic.

--- Loops/PalindromeNum.java
+++ Loops/PalindromeNum.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class PalindromeNum {
 	public static void main(String[] args) {
-		// Finds whether the given number is Palindrome number or not by reversing the number using while loop.
+																		// Finds whether the given number is Palindrome number or not by reversing the number using while loop.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number : ");
 		int num = sc.nextInt();
 		int temp = num;
 		int reverse = 0;
 		sc.close();
-		while(num > 0) {
-			int digit = num % 10;
-			reverse = reverse * 10 + digit;
+		while(num > 0) {												//While loop.
+			int digit = num % 10;										//Derives reminder of the num which is last digit of the number.
+			reverse = reverse * 10 + digit;								//Storing the last digit in reverse by multiplying the reverse with 10 and adding digit to it.
 			num /= 10;
 		}
 		String result = (temp == reverse) ? "a Palindrome" : "not a Palindrome";
