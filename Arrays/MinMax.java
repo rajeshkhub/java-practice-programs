66d40635c3e1585544d2b68cd1e2ee9a3ab40bc3
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-09 19:14:00 +0530

    Refactor MinMax class for clarity and structure

--- Arrays/MinMax.java
+++ Arrays/MinMax.java
@@ -5,35 +5,43 @@ public class MinMax {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the size of array : ");
 		int size = sc.nextInt();
-		int[] array = new int[size];																//Assigning size to array.
+		int[] array = new int[size];																// Assigning size to array.
 		System.out.println("Enter your elements.");
-		for(int i = 0; i < size ; i++) {															//For loop to insert input elements in array.
+		for(int i = 0; i < size ; i++) {															// For loop to insert input elements in array.
 			array[i] = sc.nextInt();
 		}
 		System.out.print("Your array elements are : ");
-		for(int i = 0; i < array.length ;i++) {														//For loop to print array elements.
+		for(int i = 0; i < array.length ;i++) {														// For loop to print array elements.
 			System.out.print(array[i]+" ");
 		}
-		while(true) {																				//While loop (continues loop if user gives invalid input).
+		while(true) {																				// While loop (continues loop if user gives invalid input).
 			System.out.println("\nChoose your option : \n1. Maximum element \n2. Minimum element");
-																									//Max block
+																									// Max block
 			int choice = sc.nextInt();
-			if(choice == 1) { int max = array[0];
-			for(int i = 0; i < array.length;i++) {
-				if(array[i] > max) {
-					max = array[i];
+			if(choice == 1) {
+				int max = array[0];
+				for(int i = 0; i < array.length;i++) {
+					if(array[i] > max) {
+						max = array[i];
+					}
 				}
+				System.out.println("\nMax of the array : "+max);
+				break;
 			}
-			System.out.println("\nMax of the array : "+max); break;}
-																									//MIN block
-			else if(choice == 2) {int min = array[0];
-			for(int i = 0 ; i < array.length ;i++) {
-				if(array[i] < min) {
-					min = array[i];
+																									// MIN block
+			else if(choice == 2) {
+				int min = array[0];
+				for(int i = 0 ; i < array.length ;i++) {
+					if(array[i] < min) {
+						min = array[i];
+					}
 				}
+				System.out.println("\nMin of the array : "+min);
+				break;
 			}
-			System.out.println("\nMin of the array : "+min); break;}
-			else {System.out.println("Invalid input!");}
+			else {
+				System.out.println("Invalid input!");
+			}
 		}
 		sc.close();
 	}
