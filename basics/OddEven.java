05f0c12375bf348c0d0844aa898594da43bc1882
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-09 18:58:03 +0530

    Refactor OddEven program with labeled loops
    Refactor OddEven program to improve structure and readability with labeled loops.

--- basics/OddEven.java
+++ basics/OddEven.java
@@ -1,32 +1,33 @@
 import java.util.*;
 public class OddEven {
 	public static void main(String[] args) {
-		// Finds whether the number is odd or even number.
+																			// Finds whether the number is odd or even number.
 		Scanner sc = new Scanner(System.in);
-while(true){try{
-System.out.println("Enter a number : ");
-int num = sc.nextInt();
-String word = (num % 2 == 0)? "Even" : "Odd";
-System.out.println("The given number "+num+" is : "+word);
-}
-catch(InputMismatchException e){
-	System.out.println("Invalid input!");
-	sc.next();
-	continue;
-}
-String word2;
-while(true) {
-System.out.println("Do u want to continue : yes/no");
-word2 = sc.next().toLowerCase();
-if(word2.equals("yes")) break;
-else if(word2.equals("no")) break;
-else {
-	System.out.println("Invalid answer.");
-	  continue;
-	 }
-}
-if(word2.equals("no")) break;}
-System.out.println("Thank you");
-sc.close();
+		outer : while(true){												// While loop to continue program.
+			try{															// Try block to throws exception.
+				System.out.println("Enter a number : ");
+				int num = sc.nextInt();
+				String word = (num % 2 == 0)? "Even" : "Odd";				// Terinary operator for result.
+				System.out.println("The given number "+num+" is : "+word);
+			}
+			catch(InputMismatchException e){								// Catch block handles the exception thrown from try block.
+				System.out.println("Invalid input!");
+				sc.next();
+				continue;
+			}
+			inner : while(true) {											// While loop to control continuation of the program.
+				System.out.println("Do u want to continue : yes/no");
+				String word2 = sc.next().toLowerCase();
+				if(word2.equals("yes")) break inner;
+				else if(word2.equals("no")){
+					System.out.println("Thank you")
+					break outer;
+				}
+				else {
+					System.out.println("Invalid answer");
+					continue;
+				}
+			}
+		}
 	}
 }
