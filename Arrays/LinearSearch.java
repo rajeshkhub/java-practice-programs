9192ac2a1623e4e4dfe93a6b791fb78809a332b2
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-09 19:10:10 +0530

    Refactor LinearSearch for clarity and consistency

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -5,26 +5,26 @@ public class LinearSearch {
                Scanner sc = new Scanner(System.in);
                System.out.println("Enter the array size : ");
                int size = sc.nextInt();
-               int array[] = new int[size];												//Assigning size to the array.
+               int array[] = new int[size];												// Assigning size to the array.
                System.out.println("Enter your array elements : ");
-               for(int i = 0; i < size; i++) {											//For loop to insert input numbers into the array.
+               for(int i = 0; i < size; i++) {											// For loop to insert input numbers into the array.
 	                   array[i] = sc.nextInt();
                }
                System.out.print("Your array elements are : ");
-               for(int i = 0; i < array.length; i++) {									//For loop to print array elements.
+               for(int i = 0; i < array.length; i++) {									// For loop to print array elements.
                        System.out.print(array[i]+" ");
 			   }
                System.out.println("\nWhich element you want to search : ");
-               int target = sc.nextInt();												//target element.
+               int target = sc.nextInt();												// Target element.
                boolean found = false;													
-               for(int i = 0; i < array.length; i++) {									//For loop for finding target element in the array.
+               for(int i = 0; i < array.length; i++) {									// For loop for finding target element in the array.
 	                     if(target == array[i]) {
-		                         System.out.println("Element found at index : "+i);		//prints index of target element in the array.
-                                 found = true;											//found becomes true when target element is found.
+		                         System.out.println("Element found at index : "+i);		// Prints index of target element in the array.
+                                 found = true;											// Found becomes true when target element is found.
                          break;
 						 }
                }
-   if(!found){																			//condition if target is not found.
+   if(!found){																			// Condition if target is not found.
 	   System.out.println("Element not found in the list.");
    }
    sc.close();
