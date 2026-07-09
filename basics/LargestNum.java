ec5019b9c39df1c3d55948046b0e0d4017d2d27e
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-09 18:33:46 +0530

    Refactor LargestNum.java with comments and formatting

--- basics/LargestNum.java
+++ basics/LargestNum.java
@@ -3,20 +3,22 @@ public class LargestNum {
 	public static void main(String[] args) {
 																									// Finds largest/ maximum number in three numbers.
            Scanner sc = new Scanner(System.in);
-           while(true) {
-			   try { 
+           while(true) {																			// While loop.
+			   try { 																				// Try block.
 				   System.out.println("Enter three numbers : ");
 				   int num1 = sc.nextInt();
 				   int num2 = sc.nextInt();
 				   int num3 = sc.nextInt();
 				   if(num1 > num2 && num1 > num3) System.out.println("The largest number among them is : "+num1);
-                     else if(num2 > num3) System.out.println("The largest number among them is : "+num2);
-                     else if(num1 == num2 && num2 == num3) System.out.println("All numbers are equal.");
-                     else System.out.println("The largest number among them is : "+num3);
-					break;
-			  }
-              catch(InputMismatchException e) { System.out.println("Invalid input!");
-             sc.next();}
+				   else if(num2 > num3) System.out.println("The largest number among them is : "+num2);
+				   else if(num1 == num2 && num2 == num3) System.out.println("All numbers are equal.");
+				   else System.out.println("The largest number among them is : "+num3);
+				   break;
+			   }
+			   catch(InputMismatchException e) { 													// Catch block to handle exception.
+				   System.out.println("Invalid input!");
+				   sc.next();
+			   }
 		   }
 		sc.close();
 	}
