d31a67b611dccee0fd958d824419f29f49c2c5f2
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-10 22:51:25 +0530

    Refactor AnagramCheck for clarity and consistency

--- Strings/AnagramCheck.java
+++ Strings/AnagramCheck.java
@@ -7,23 +7,23 @@ public class AnagramCheck {
 		String word = sc.next().toLowerCase();
 		String word2 = sc.next().toLowerCase();
 		boolean found = false;
-		boolean[] visited = new boolean[word2.length()];               //Boolean array to store visited or not, in indexes parallel to the second string indexes.
-		if(word.length() == word2.length()) {                          //Checks whether the two strings have equal lenghts.
-			for(int i = 0; i < word.length(); i++) {                   //Iteration over first string.
+		boolean[] visited = new boolean[word2.length()];               // Boolean array to store visited or not, in indexes parallel to the second string indexes.
+		if(word.length() == word2.length()) {                          // Checks whether the two strings have equal lenghts.
+			for(int i = 0; i < word.length(); i++) {                   // Iteration over first string.
 				found = false;
-				for(int j = 0; j < word2.length(); j++) {              //Iteration over second string.
-					if(!visited[j]) {                                  //Checks if the boolean array element at index j is visited or not.
-						if(word.charAt(i) == word2.charAt(j)) {        //Compares character of first string at index i with character of second string with index j.
-							found = true;                              //Assigns true to found if the above condition is true.
+				for(int j = 0; j < word2.length(); j++) {              // Iteration over second string.
+					if(!visited[j]) {                                  // Checks if the boolean array element at index j is visited or not.
+						if(word.charAt(i) == word2.charAt(j)) {        // Compares character of first string at index i with character of second string with index j.
+							found = true;                              // Assigns true to found if the above condition is true.
 							visited[j] = true;                         // Marks the visited array true at index j.
 							break;
 						}
 					}
 				}
-				if(!found) break;                                       //checks if found is false(found becomes false if any element doesnt match) and breaks the loop.
+				if(!found) break;                                       // Checks if found is false(found becomes false if any element doesnt match) and breaks the loop.
 			}
 		}
-		String result = (found)? "Anagrams" : "Not Anagrams";      		 //Generates result using terinary operator.
+		String result = (found)? "Anagrams" : "Not Anagrams";      		 // Generates result using terinary operator.
 		System.out.println("The given two strings are "+result);
 		
 	}
