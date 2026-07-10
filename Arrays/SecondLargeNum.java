7be4e2493518fb32c8e97a8d7b6593950c3af090
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-10 22:47:45 +0530

    Refactor code for finding second largest number

--- Arrays/SecondLargeNum.java
+++ Arrays/SecondLargeNum.java
@@ -1,30 +1,30 @@
 import java.util.*;
 public class SecondLargeNum {
 	public static void main(String[] args) {
-																//Finds second largest number in the array but finding first largest number and removing it.
+																// Finds second largest number in the array but finding first largest number and removing it.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter array size : ");
 		int size = sc.nextInt();
-		int array[] = new int[size];							//Assigning size to array.
+		int array[] = new int[size];							// Assigning size to array.
 		System.out.println("Enter array elements : ");
-		for(int i = 0; i < size;i++) {							//For loop to insert input numbers into the array.
+		for(int i = 0; i < size;i++) {							// For loop to insert input numbers into the array.
 			array[i] = sc.nextInt();
 		}
 		sc.close();
-																//finding first maximum number
+																// Finding first maximum number
 		int firstMax = array[0];
 		for(int i = 0; i < array.length;i++) {
 			if(array[i] > firstMax) {
 				firstMax = array[i];
 			}
 		} 
-																//Removing first maximum number from array
+																// Removing first maximum number from array
 		for(int i = 0 ; i < array.length;i++) {
 			if(array[i] == firstMax) {
 				array[i] -= firstMax;
 			}
 		}
-																//finding current maximum number in the array
+																// Finding current maximum number in the array
 		int secondMax = array[0];
 		for(int i = 0 ; i < array.length;i++) {
 			if(array[i] > secondMax) {
