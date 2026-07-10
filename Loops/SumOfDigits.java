82c8d2420aba2ae08945b8cc5872a6d1007a7d7f
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-10 22:49:41 +0530

    Format code in SumOfDigits.java

--- Loops/SumOfDigits.java
+++ Loops/SumOfDigits.java
@@ -6,10 +6,10 @@ public class SumOfDigits {
 		System.out.println("Enter a number : ");
 		int num = sc.nextInt();
 		int sum = 0;
-															//Using While loop.
+															// Using While loop.
 		while(num > 0) {
-			sum += num % 10;								//Digit derived by num modulo 10 is added to sum.
-			num /= 10;										//num divided by 10 removes the last digit.
+			sum += num % 10;								// Digit derived by num modulo 10 is added to sum.
+			num /= 10;										// num divided by 10 removes the last digit.
 		}
 		System.out.println("Sum of digits : "+sum);
 		sc.close();
