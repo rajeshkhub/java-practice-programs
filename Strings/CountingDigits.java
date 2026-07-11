5ad4468e2c6a742678a7f1263a92277174df88f3
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-11 21:49:57 +0530

    Format CountingDigits.java and clean comments

--- Strings/CountingDigits.java
+++ Strings/CountingDigits.java
@@ -6,19 +6,19 @@ public class CountingDigits {
 		System.out.println("Enter your number : ");
 		int num = sc.nextInt();
 		sc.close();
-																//Storing input in string variable.
+																// Storing input in string variable.
 		/*String a = sc.next();
 		 * System.out.println("Number of digits in the number are : "+a.length());
 		 */
-																//Using string methods.
-		/*int length = String.valueOf(num).length();			//Converts int num variable into string and generates the length of it.
+																// Using string methods.
+		/*int length = String.valueOf(num).length();			// Converts int num variable into string and generates the length of it.
 		System.out.println("No.of digits : "+length);
 		*/
-																//Using while loop.
+																// Using while loop.
 		int count = 1;
-		while(num >= 10) {										//While loop runs until num is equals to or less than 10.
-			num /= 10;                                          //num is divided by 10.
-			count++;                                            //Count increment.
+		while(num >= 10) {										// While loop runs until num is equals to or less than 10.
+			num /= 10;                                          // num is divided by 10.
+			count++;                                            // Count increment.
 		}
 		System.out.println("No.of digits : "+count);
 	}
