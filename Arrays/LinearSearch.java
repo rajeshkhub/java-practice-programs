43ef8db153897e71ad58e7e01758de32b293ac40
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-11 23:04:36 +0530

    Refactor LinearSearch.java for clarity and formatting

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -8,22 +8,22 @@ public class LinearSearch {
                int array[] = new int[size];												// Assigning size to the array.
                System.out.println("Enter your array elements : ");
                for(int i = 0; i < size; i++) {											// For loop to insert input numbers into the array.
-	                   array[i] = sc.nextInt();
-               }
+				   array[i] = sc.nextInt();
+			   }
                System.out.print("Your array elements are : ");
                for(int i = 0; i < array.length; i++) {									// For loop to print array elements.
-                       System.out.print(array[i]+" ");
+				   System.out.print(array[i]+" ");
 			   }
                System.out.println("\nWhich element you want to search : ");
                int target = sc.nextInt();												// Target element.
                boolean found = false;													
                for(int i = 0; i < array.length; i++) {									// For loop for finding target element in the array.
-	                     if(target == array[i]) {
-		                         System.out.println("Element found at index : "+i);		// Prints index of target element in the array.
-                                 found = true;											// Found becomes true when target element is found.
-                         break;
-						 }
-               }
+				   if(target == array[i]) {
+					   System.out.println("Element found at index : "+i);				// Prints index of target element in the array.
+					   found = true;													// Found becomes true when target element is found.
+					   break;
+				   }
+			   }
 		if(!found){																		// Condition if target is not found.
 			System.out.println("Element not found in the list.");
 		}
