62f6b32b63a5ee08d1702f0ce49e8d5a17c44e9e
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-11 23:12:07 +0530

    Refactor VowelCount.java for clarity and formatting

--- Strings/VowelCount.java
+++ Strings/VowelCount.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 class VowelCount {
 	public static void main(String[] args) {
-																												// Counts number of vowels in a string.
+																											// Counts number of vowels in a string.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter any string/word : ");
-			String word = sc.nextLine().toLowerCase();															// Turns the input string into lower case.
-			int count = 0;
-			char[] letters = word.toCharArray();       															// String becomes array of characters.
-			for(char vowels : letters) {                                    								    // for each loop.
-				if(vowels == 'a' || vowels == 'e' || vowels == 'i' || vowels == 'o' || vowels == 'u') {         // Compares characters with vowels.
-					count++;																					// Count increment.
-				}
+		String word = sc.nextLine().toLowerCase();															// Turns the input string into lower case.
+		int count = 0;
+		char[] letters = word.toCharArray();       															// String becomes array of characters.
+		for(char vowels : letters) {                                    								    // for each loop.
+			if(vowels == 'a' || vowels == 'e' || vowels == 'i' || vowels == 'o' || vowels == 'u') {         // Compares characters with vowels.
+				count++;																					// Count increment.
 			}
-			System.out.println("Number of vowels in the given string : "+count);
-			sc.close();
+		}
+		System.out.println("Number of vowels in the given string : "+count);
+		sc.close();
 	}
 }
