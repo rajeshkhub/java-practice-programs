f13c744911872c1c407197b7652e4a2d61bbf6d4
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-12 21:32:43 +0530

    Enhance Daimond.java with explanatory comments
    Added comments to clarify the purpose of the code and its logic.

--- Patterns/Daimond.java
+++ Patterns/Daimond.java
@@ -1,28 +1,26 @@
 import java.util.Scanner;
 public class Daimond {
 	public static void main(String[] args) {
-		// Generates Daimond shaped pattern.
+																// Daimond pattern.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter height : ");
-		int height = sc.nextInt();
+		int height = sc.nextInt();								// Height is equals to number of rows of pattern.
 		sc.close();
-		int mid = (height+1)/2;
-		for(int i = 1; i <= height; i++) {
-			int level;
-			if(i <= mid) {
-				level = i;
-			}
+		int mid = (height+1)/2;									// Mid of the height.
+		
+		for(int i = 1; i <= height; i++) {						// Outer/row loop for height of the pattern.
+			int level;											// For number of iterations of the inner loops.
+			if(i <= mid) level = i;
 			else level = height - (i - 1);
-			// Space loop
-			for(int j = height-level; j >= mid; j--) {
+			
+			for(int j = height-level; j >= mid; j--) {			// First inner loop for spaces.
 				System.out.print(" ");
 			}
-			// Star loop
-			for(int j = 1; j <= 2*level-1 ; j++) {
+														
+			for(int j = 1; j <= 2*level-1 ; j++) {				// Second inner loop to print "*".
 				System.out.print("*");
 			}
 			System.out.println();
-			}
+		}
 	}
-
 }
