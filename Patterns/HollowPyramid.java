4f1dc973cb543e2baacaadca43e2adb0815fedef
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-12 21:37:17 +0530

    Enhance comments in HollowPyramid.java
    Added comments to clarify the purpose of the code and its components.

--- Patterns/HollowPyramid.java
+++ Patterns/HollowPyramid.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 public class HollowPyramid {
 	public static void main(String[] args) {
-    // Prints Hollow pyramid.
+    															// Hollow pyramid pattern.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter height :");
-		int height = sc.nextInt();
+		int height = sc.nextInt();								// Height of the pattern.
 		sc.close();
-		for(int i = 1; i <= height; i++) {
-			for(int j = 1; j <= height-i; j++) {
+		
+		for(int i = 1; i <= height; i++) {						// Outer/row loop for height of the pattern.
+			
+			for(int j = 1; j <= height-i; j++) {				// First inner loop to print spaces.
 				System.out.print(" ");
 			}
-			for(int j = 1; j <= 2*i-1; j++) {
-				if(i == height || j == 1 || j == 2*i-1) {
-						System.out.print("*");
-				}
-				else System.out.print(" ");
+			
+			for(int j = 1; j <= 2*i-1; j++) {					// Second inner loop.
+				if(i == height || j == 1 || j == 2*i-1) System.out.print("*");  // Prints "*" if condition becomes true.
+				else System.out.print(" ");						// Prints spaces if condition becomes false.
 			}
 			System.out.println();
 		}
