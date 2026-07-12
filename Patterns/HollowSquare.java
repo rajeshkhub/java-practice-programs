276a2009fa1d3844c1785597cab2e9cec9613e37
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-12 21:44:05 +0530

    Refine comments in HollowSquare.java
    Updated comments for clarity and consistency.

--- Patterns/HollowSquare.java
+++ Patterns/HollowSquare.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class HollowSquare {
 	public static void main(String[] args) {
-    // Prints Hollow Square pattern.
+    																	// Hollow Square pattern.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter height :");
-		int height = sc.nextInt();
+		int height = sc.nextInt();										// Height of the pattern.
 		sc.close();
-for(int i = 1; i <= height; i++) {
-			for(int j = 1; j <= height; j++) {
-				if(i == 1 || i == height || j == 1 || j == height) {
-				System.out.print("*");
-				}
-				else System.out.print(" ");
+		
+		for(int i = 1; i <= height; i++) {								// Outer/row loop for height of pattern.
+			
+			for(int j = 1; j <= height; j++) {							// Inner/column loop to print stars and spaces. 
+				if(i == 1 || i == height || j == 1 || j == height) System.out.print("*");	// If condition is true prints stars.
+				else System.out.print(" ");								// If condition is false then prints spaces.
 			}
 			System.out.println();
 		}
