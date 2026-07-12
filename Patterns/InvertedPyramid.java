7532160f82197f07a653d70a461b8e2ec5a04a7d
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-12 21:50:48 +0530

    Refactor Inverted Pyramid pattern implementation
    Refactored inverted pyramid pattern logic and improved comments.

--- Patterns/InvertedPyramid.java
+++ Patterns/InvertedPyramid.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 public class InvertedPyramid {
 	public static void main(String[] args) {
-//Prints inverted pyramid for the given height.
-Scanner sc = new Scanner(System.in);
+															// Inverted pyramid pattern.
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter height :");
-int height = sc.nextInt();
-sc.close();
-		for(int i = 1; i <= height; i++) {
-			//space loop
-			for(int j = 1; j < i; j++) {
+		int height = sc.nextInt();							// Height of the pyramid.
+		sc.close();
+		
+		for(int i = height; i > 0; i--) {					// Outer/row loop for height of the pattern.
+			
+			for(int j = i; j < height-1; j++) {				// First inner loop to print spaces.
 				System.out.print(" ");
 			}
-			//star loop
-			for(int j = i; j <= 2*height-i; j++) {
+		
+			for(int j = 1; j <= 2*i-1; j++) {				// Second inner loop to print stars.
 				System.out.print("*");
 			}
 			System.out.println();
