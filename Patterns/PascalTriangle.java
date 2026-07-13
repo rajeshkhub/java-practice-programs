eb3aac1943d2cb04a9f283c9406fabb2ed782ff8
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-13 22:33:28 +0530

    Improve readability of PascalTriangle.java
    Refactor variable names for clarity and add comments to explain functionality.

--- Patterns/PascalTriangle.java
+++ Patterns/PascalTriangle.java
@@ -1,24 +1,27 @@
 import java.util.Scanner;
 public class PascalTrangle {
 	public static void main(String[] args) {
-		// Pascal Triangle pattern.
+																	// Pascal Triangle pattern.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter height :");
-		int height = sc.nextInt();
+		int height = sc.nextInt();									// Height of the pattern.
 		sc.close();
-    int[] prev = null;
-		for(int i = 1; i <= height; i++) {
-			for(int j = 1; j <= height-i; j++) {
+		int[] previous = null;										// Integer array (previous) to store previous row numbers. 
+		
+		for(int i = 1; i <= height; i++) {							// Outer/row loop for pattern height.
+			
+			for(int j = 1; j <= height-i; j++) {					// First inner loop to print spaces.
 				System.out.print(" ");
 			}
-			int[] current = new int[i];
-			for(int j = 1; j <= i; j++) {
-				if(j == i || j == 1) current[j-1] = 1;
-				else current[j-1] = prev[j-2]+prev[j-1];
+			int[] current = new int[i];								// Integer array (current) to print current row elements.
+			
+			for(int j = 1; j <= i; j++) {							// Second inner loop to print numbers.
+				if(j == i || j == 1) current[j-1] = 1;				// if condition is true then "1" is inserted into current array.
+				else current[j-1] = previous[j-2]+previous[j-1];	// else previous array elements at index will be added and inserted into the current array.
 				System.out.print(current[j-1]+" ");
 			}
-			prev = current;
+			previous = current;										// previous array points to current array elements.
 			System.out.println();
 		}
-  }
+	}
 }
