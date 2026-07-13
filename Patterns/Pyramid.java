5ee08e7fd99738bac26e31152318ad114796b877
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-13 22:21:39 +0530

    Improve pyramid pattern implementation
    Refactor pyramid pattern code for clarity and structure.

--- Patterns/Pyramid.java
+++ Patterns/Pyramid.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 public class Pyramid {
 	public static void main(String[] args) {
-		//Pyramid pattern
+																//Pyramid pattern.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Pyramid height : ");
-		int height = sc.nextInt();
+		int height = sc.nextInt();								// Height of the pattern.
 		sc.close();
-		for(int i = 1; i <= height; i++) {             //Outer loop
-			for(int j = 1; j <= height-i; j++) {       //Space loop
+		
+		for(int i = 1; i <= height; i++) {						// Outer/row loop for pattern height.
+			
+			for(int j = i; j < height-1; j++) {       			// First inner loop to print spaces.
 				System.out.print(" ");
 			}
-			for(int k = 1; k <= 2*i-1; k++) {          //Star loop
+			
+			for(int j = 1; j <= 2*i-1; j++) {          			// Second inner loop to print stars.
 				System.out.print("*");
 			}
 			System.out.println();
