ea26b8c85f120c23d508e5f268bf3fc38165d168
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-14 20:02:08 +0530

    Implement first non-repeating character logic
    Add initial implementation for finding the first non-repeating character in a string.

---
+++ Strings/FirstNon-RepeatingChar.java
@@ -0,0 +1,17 @@
+//First non repeating character
+//		BOOLEAN FOUND = FALSE;
+//		FOR(INT I = 0; I < WORD.LENGTH(); I++) {
+//			FOUND = FALSE;
+//			FOR(INT J = 0; J < WORD.LENGTH(); J++) {
+//				IF(I == J) CONTINUE;
+//					IF(CHARACTER.TOLOWERCASE(WORD.CHARAT(I)) == CHARACTER.TOLOWERCASE(WORD.CHARAT(J))) {
+//						FOUND = TRUE;
+//						BREAK;
+//					}
+//			}
+//			IF(!FOUND) {
+//				SYSTEM.OUT.PRINT(WORD.CHARAT(I));
+//				BREAK;
+//			}
+//		}
+//		if(found) System.out.print("No non repeating character found");
