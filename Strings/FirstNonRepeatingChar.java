edb7eb933354db7641e17c977a0ae5c96e65db5e
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-14 20:33:37 +0530

    Refactor FirstNonRepeatingChar logic and comments
    Updated comments for clarity and corrected logic for finding non-repeating characters.

--- Strings/FirstNonRepeatingChar.java
+++ Strings/FirstNonRepeatingChar.java
@@ -1,26 +1,28 @@
 import java.util.*;
 public class FirstNonRepeatingChar {
 	public static void main(String[] args) {
-																				//First non repeating character
+																				// First non repeating character finder.
 		Scanner sc = new Scanner(System.in);
-		boolean found = false;
+		System.out.println("Enter a word/string : ");
+		String word = sc.next();
+		boolean found = true;													// boolean found used to find if there is any non repeating character.
 		
-		for(int i = 0; i < word.length(); i++) {
-			found = false;
+		for(int i = 0; i < word.length(); i++) {								// for loop for iteration over word.
+			found = true;
 			
-			for(int j = 0; j < word.length(); j++) {
-				if(i == j) continue;
+			for(int j = 0; j < word.length(); j++) {							// Inner for loop to iterate over word by comparing i and j index character.
+				if(i == j) continue;											// skips if both i and j or on the same character.
 				if(Character.toLowerCase(word.charAt(i)) == Character.toLowerCase(word.charAt(j))) {
-					found = true;
+					found = false;												// if condition becomes true then found will be false.
 					break;
 				}
 			}
-			if(!found) {
-				System.out.print(word.charAt(i));
+			if(found) {															// if found is true then it prints first non repeating character.
+				System.out.print("First non repeating character : "+word.charAt(i));
 				break;
 			}
 		}
-		if(found) System.out.print("No non repeating character found");
+		if(!found) System.out.print("No non repeating character found");		// if found is false then it prints the statement.
 		sc.close();
 	}
 }
