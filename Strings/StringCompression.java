fe57415cb8d09b13d0fa9b25d6ddc71cd486af8a
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-15 21:55:40 +0530

    Improve string compression algorithm structure
    Refactor string compression logic for clarity and functionality.

--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -1,16 +1,18 @@
 import java.util.*;
 public class StringCompression {
 	public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a string : ");
+    													// Compresses string by replacing consecutive repeated char's with char followed by its count.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string : ");
 		String word = sc.next();
-    int num = 1;
-		for(int i = 0; i < word.length(); i++) {
-      int count = 1;
-			for(int j = num; j < word.length(); j++) {
-        if(word.charAt(i) == word.charAt(j)) {
-					count++;
+		int num = 1;									// Used to store index to start with new group of characters.
+		
+		for(int i = 0; i < word.length(); i++) {		// Outer loop starts with 0 index.
+			int count = 1;								// Used to count the number of consecutive repeating characters.
+			
+			for(int j = num; j < word.length(); j++) {	// Inner loop starts with index of num value.
+				if(word.charAt(i) == word.charAt(j)) {	// Comparing characters.
+					count++;							// Count increment if characters are same.
 					num = j+2;
 				}
 				else {
