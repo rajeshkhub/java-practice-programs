ae7c9c84404a67c29921b2f6707f986e781ca1b7
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-16 20:39:43 +0530

    Refactor LongestWord program for clarity and prompts

--- Strings/LongestWord.java
+++ Strings/LongestWord.java
@@ -1,18 +1,19 @@
 import java.util.*;
 public class LongestWord {
   public static void main(String[] args) {
-                                                  // Longest word in a sentence.
-    Scanner sc = new Scanner(System.in);
-    String sentence = sc.nextLine().trim();
-		String[] words = sentence.split(" ");        // String array stores words in the sentence.
-		String oneWord = words[0];
+                                                  	// Longest word in a sentence.
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("Enter a sentence : ");
+	  String sentence = sc.nextLine().trim();
+	  String[] words = sentence.split(" ");        	// String array stores words in the sentence.
+	  String oneWord = words[0];
     
-		for(String word : words) {                  // For each loop. 
-      if(oneWord.length() < word.length()) {
-        oneWord = word;
-			}
-		}
-		System.out.println(oneWord);
-    sc.close();
+	  for(String word : words) {                  	// For each loop. 
+		  if(oneWord.length() < word.length()) {	// Comparing length of words.
+			  oneWord = word;						// Stores longest word.
+		  }
+	  }
+	  System.out.println(oneWord);
+	  sc.close();
   }
 }
