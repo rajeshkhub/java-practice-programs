4b188bdbbefec92db54bc860a73f3a0f60984811
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-17 16:38:24 +0530

    Rename class and enhance string rotation check
    Refactor class name and improve output messages for clarity.

--- Strings/StringRotationCheck.java
+++ Strings/StringRotationCheck.java
@@ -1,13 +1,15 @@
 import java.util.*;
-public class StrIngs2 {
+public class StringRotationCheck {
 	public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
+    																	// Strings rotation checker.	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter main string : ");
 		String word1 = sc.next();
+		System.out.println("Enter rotated string : ");
 		String word2 = sc.next();
-		String word3 = word1 + word1;
+		String word3 = word1 + word1;									// Concatenates word1 with itself and checks for word2 in it.
 		if(word3.contains(word2)) System.out.println(word2+" is Rotation of "+word1);
-		else System.out.println("No");
-    sc.close();
+		else System.out.println(word2+" is Not rotation of "+word1);
+		sc.close();
 	}
 }
