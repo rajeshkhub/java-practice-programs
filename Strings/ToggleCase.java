f38856cd650f639518ff162c88c6dc5ca41a939e
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-18 19:23:10 +0530

    Add toggle case functionality to ToggleCase class

--- Strings/ToggleCase.java
+++ Strings/ToggleCase.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class ToggleCase {
 	public static void main(String[] args) {
-    
+    														// Toggle case.
 		Scanner sc = new Scanner(System.in);
 		String word = sc.nextLine();
 		for(int i = 0; i < word.length(); i++) {
@@ -9,6 +9,6 @@ public class ToggleCase {
 			if(Character.isUpperCase(letter)) System.out.print(Character.toLowerCase(letter));
 			else System.out.print(Character.toUpperCase(letter));
 		}
-    sc.close();
+		sc.close();
 	}
 }
