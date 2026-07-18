cbc24d1cb8080c19b66c79904177a11b0ff39dd5
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-18 19:26:05 +0530

    Enhance ToggleCase with input/output prompts
    Added user prompts for input and output.

--- Strings/ToggleCase.java
+++ Strings/ToggleCase.java
@@ -3,7 +3,9 @@ public class ToggleCase {
 	public static void main(String[] args) {
     														// Toggle case.
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string :");
 		String word = sc.nextLine();
+		System.out.print("Toggled string : ");
 		for(int i = 0; i < word.length(); i++) {
 			char letter = word.charAt(i);
 			if(Character.isUpperCase(letter)) System.out.print(Character.toLowerCase(letter));
