c8a186bb710d5551d2341142b3a8440b31566bda
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-19 10:36:29 +0530

    Add comments to ToggleCase logic
    Added comments to the ToggleCase.java code for clarity.

--- Strings/ToggleCase.java
+++ Strings/ToggleCase.java
@@ -6,10 +6,10 @@ public class ToggleCase {
 		System.out.println("Enter a string :");
 		String word = sc.nextLine();
 		System.out.print("Toggled string : ");
-		for(int i = 0; i < word.length(); i++) {
-			char letter = word.charAt(i);
-			if(Character.isUpperCase(letter)) System.out.print(Character.toLowerCase(letter));
-			else System.out.print(Character.toUpperCase(letter));
+		for(int i = 0; i < word.length(); i++) {			// for loop to iterate over word.
+			char letter = word.charAt(i);					// aquiring single characters from string and storing in letter.
+			if(Character.isUpperCase(letter)) System.out.print(Character.toLowerCase(letter));		// using static method of Character wrapper class.
+			else System.out.print(Character.toUpperCase(letter));									// to check and print toggled character.
 		}
 		sc.close();
 	}
