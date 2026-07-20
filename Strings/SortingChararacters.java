69c8214071cb3d803fa67270cfe526feb4a989f7
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-20 21:29:16 +0530

    Improve comments for clarity in SortingCharacters.java

--- Strings/SortingChararacters.java
+++ Strings/SortingChararacters.java
@@ -5,19 +5,19 @@ public class SortingCharacters {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a word/string : ");
 		String word = sc.next();
-		char[] letters = word.toCharArray();				// word converted into character array.
+		char[] letters = word.toCharArray();				// Word converted into character array.
 		
 		for(int i = 0; i < letters.length; i++) {			// for loop to iterate over character array.
 			
-			for(int j = i+1; j < letters.length; j++) {		// inner loop to iterate over character array from i+1 index.
-				char letter = letters[i];					// each character is stored in letter variable.
-				if(letters[j] < letters[i]) {				// checks if character at index j is greater than character at index i.
-					letters[i] = letters[j];				// character at index j is stored at index i.
-					letters[j] = letter;					// the letter variable which stored character at index i is stored at index j.
+			for(int j = i+1; j < letters.length; j++) {		// Inner loop to iterate over character array from i+1 index.
+				char letter = letters[i];					// Each character is stored in letter variable.
+				if(letters[j] < letters[i]) {				// Checks if character at index j is greater than character at index i.
+					letters[i] = letters[j];				// Character at index j is stored at index i.
+					letters[j] = letter;					// letter variable which stored character at index i is stored at index j.
 				}
 			}
 		}
-		String word1 = new String(letters);					// character array is converted to string object.
+		String word1 = new String(letters);					// Character array is converted to string object.
 		System.out.print("String after sorting : "+word1);
 		sc.close();
 	}
