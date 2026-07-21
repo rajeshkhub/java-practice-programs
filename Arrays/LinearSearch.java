4a9102fa0b5bc04e57cd05bc186fadba8d0aebba
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-21 20:29:48 +0530

    Refactor LinearSearch.java for clarity and formatting

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -7,16 +7,20 @@ public class LinearSearch {
 		int size = sc.nextInt();
 		int array[] = new int[size];											// Assigning size to the array.
 		System.out.println("Enter your array elements : ");
+		
 		for(int i = 0; i < size; i++) {											// For loop to insert input numbers into the array.
 			array[i] = sc.nextInt();
 		}
+		
 		System.out.print("Your array elements are : ");
+		
 		for(int i = 0; i < array.length; i++) {									// For loop to print array elements.
 			System.out.print(array[i]+" ");
 		}
 		System.out.println("\nWhich element you want to search : ");
 		int target = sc.nextInt();												// Target element.
-		boolean found = false;													
+		boolean found = false;
+		
 		for(int i = 0; i < array.length; i++) {									// For loop for finding target element in the array.
 			if(target == array[i]) {
 				System.out.println("Element found at index : "+i);				// Prints index of target element in the array.
