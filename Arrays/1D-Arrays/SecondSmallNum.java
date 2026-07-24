fe7f21107b48e8de4be6c72e8c340b386001d5f9
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-24 19:55:52 +0530

    Refactor to find second smallest number in array

--- Arrays/1D-Arrays/SecondSmallNum.java
+++ Arrays/1D-Arrays/SecondSmallNum.java
@@ -1,28 +1,25 @@
 import java.util.*;
 public class SecondSmallNum {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int[] arr = new int[size];
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = sc.nextInt();
 		}
-		int min = arr[0];
-		int min2 = Integer.MAX_VALUE;
+		int smallest = arr[0];
+		int secSmallest = Integer.MAX_VALUE;
 		for(int i = 1; i < arr.length; i++) {
-			if(arr[i] < min) {
-				min2 = min;
-				min = arr[i];
+			if(arr[i] < smallest) {
+				secSmallest = smallest;
+				smallest = arr[i];
 			}
-			else if(arr[i] < min2 && min != arr[i]) {
-				min2 = arr[i];
+			else if(arr[i] < secSmallest && smallest != arr[i]) {
+				secSmallest = arr[i];
 			}
 		}
 		System.out.println(min2);
-		
 		sc.close();
 	}
-
 }
