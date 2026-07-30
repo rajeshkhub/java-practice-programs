78224ba119a29fda7b035f828e235bb79c63e3a5
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-30 23:23:05 +0530

    Enhance output messages in RightRotation program

--- Arrays/1D-Arrays/RightRotation.java
+++ Arrays/1D-Arrays/RightRotation.java
@@ -3,13 +3,17 @@ public class RightRotation {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of array : ");
 		int size = sc.nextInt();
 		int[] arr = new int[size];
+		System.out.println("Enter array elements : ");
 		
 		for(int i = 0; i < size; i++)
 			arr[i] = sc.nextInt();
-			
+
+		System.out.println("Enter number of rotations : ");
 		int rotations = sc.nextInt();
+		sc.close();
 		rotations = rotations % size;
 		int[] arr2 = new int[size];
 		
@@ -24,6 +28,6 @@ public class RightRotation {
 			rotations++;
 		}
 		
-		System.out.println(Arrays.toString(arr2));
+		System.out.println("Array after right rotation : "+Arrays.toString(arr2));
 	}
 }
