f011e08cdc920d70da7d5cce77fefdea3025a0e2
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-07-30 23:33:12 +0530

    Enhance comments in RightRotation.java
    Added comments for clarity on array size, rotations, and loop functionality.

--- Arrays/1D-Arrays/RightRotation.java
+++ Arrays/1D-Arrays/RightRotation.java
@@ -4,26 +4,27 @@ public class RightRotation {
 																// Right rotation of array.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter size of array : ");
-		int size = sc.nextInt();
+		int size = sc.nextInt();								// Size of the input array.
 		int[] arr = new int[size];
 		System.out.println("Enter array elements : ");
 		
-		for(int i = 0; i < size; i++)
+		for(int i = 0; i < size; i++)							// for loop to insert array elements.
 			arr[i] = sc.nextInt();
 
 		System.out.println("Enter number of rotations : ");
-		int rotations = sc.nextInt();
-		sc.close();
-		rotations = rotations % size;
-		int[] arr2 = new int[size];
+		int rotations = sc.nextInt();							// Number of right rotations to perform on array.
+		sc.close();												// Scanner close.
+		rotations = rotations % size;							// If rotaions > size then rotations will be in range of size.
+																// Using another array and for each loop.
+		int[] arr2 = new int[size];								// Output array size equal to input array size.
 		
-		for(int num : arr) {
-			if(rotations == size) {
-				rotations = 0;
+		for(int num : arr) {									// for each loop derive input array elements each at a time.
+			if(rotations == size) {								// Checks if rotations are equal to size.
+				rotations = 0;									// if equal then no need to rotate array as it will be same.
 				arr2[rotations] = num;
 			}
-			else if(rotations < size) {
-				arr2[rotations] = num;
+			else if(rotations < size) {							// checks else if rotations less than size.
+				arr2[rotations] = num;							// If less then num element will be inserted into output array at index rotation.
 			}
 			rotations++;
 		}
