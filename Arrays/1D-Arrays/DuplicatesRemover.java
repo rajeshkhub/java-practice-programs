ca1d625e647d8fb00f75860aa6190bc956d2c562
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-04 16:12:09 +0530

    Refactor DuplicatesRemover to Array1D2 with user input
    Refactor DuplicatesRemover class to Array1D2 and add user input for array size and elements.

--- Arrays/1D-Arrays/DuplicatesRemover.java
+++ Arrays/1D-Arrays/DuplicatesRemover.java
@@ -1,4 +1,13 @@
-// Remove duplicates
+import java.util.*;
+public class Array1D2 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		for(int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
 		int[] arr2 = new int[arr.length];
 		int fill = 0;
 		for(int i = 0; i < arr.length; i++) {
