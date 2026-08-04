404cbfbc99b13dc2e066e4f537a58b988771ed8d
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-04 16:17:51 +0530

    Rename class and implement duplicates removal
    Refactored class name and added functionality to remove duplicates from an array.

--- Arrays/1D-Arrays/DuplicatesRemover.java
+++ Arrays/1D-Arrays/DuplicatesRemover.java
@@ -1,13 +1,14 @@
 import java.util.*;
-public class Array1D2 {
+public class DuplicatesRemover {
 	public static void main(String[] args) {
-		
+														// Removes duplicates in a array.
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
 		int[] arr = new int[size];
 		for(int i = 0; i < size; i++) {
 			arr[i] = sc.nextInt();
 		}
+		sc.close();
 		int[] arr2 = new int[arr.length];
 		int fill = 0;
 		for(int i = 0; i < arr.length; i++) {
@@ -20,9 +21,9 @@ public class Array1D2 {
 			}
 			if(!found) {
 				arr2[fill] = arr[i];
-				System.out.print(arr2[fill]+" ");
 				fill++;
 			}
 		}
-}
+		System.out.print(Arrays.toString(arr2));
+	}
 }
