e955931b5188ae0d1d968240d07adcc9f19b1886
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-05 20:02:07 +0530

    Enhance DuplicatesRemover.java with comments
    Added comments to clarify code functionality and logic.

--- Arrays/1D-Arrays/DuplicatesRemover.java
+++ Arrays/1D-Arrays/DuplicatesRemover.java
@@ -4,27 +4,27 @@ public class DuplicatesRemover {
 														// Removes duplicates in a array.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter array size : ");
-		int size = sc.nextInt();
+		int size = sc.nextInt();						// Size of the array.
 		int[] arr = new int[size];
 		System.out.println("Enter array elements : ");
 		
-		for(int i = 0; i < size; i++)
+		for(int i = 0; i < size; i++)					// for loop to insert elements into array.
 			arr[i] = sc.nextInt();
 		
-		sc.close();
-		int[] arr2 = new int[arr.length];
-		int fill = 0;
-		for(int i = 0; i < arr.length; i++) {
-			boolean found = false;
-			for(int j = 0; j < fill; j++) {
-				if(arr[i] == arr2[j]) {
-					found = true;
-					break;
+		sc.close();										// Scanner close.
+		int[] arr2 = new int[arr.length];				// New array for result.
+		int fill = 0;									// integer fill to use it as output array index.
+		for(int i = 0; i < arr.length; i++) {			// for loop to iterate over array.
+			boolean found = false;						// found boolean variable is initially false.
+			for(int j = 0; j < fill; j++) {				// Inner for loop for comparison.
+				if(arr[i] == arr2[j]) {					// Compares input array element with output array elements.
+					found = true;						// if same elements found then found variable becomes true.
+					break;								// And breaks the inner loop.
 				}
 			}
-			if(!found) {
-				arr2[fill] = arr[i];
-				fill++;
+			if(!found) {								// Checks if found is false.
+				arr2[fill] = arr[i];					// If found is false then inserts the input array elements into the output array at index fill.
+				fill++;									// fill increments.
 			}
 		}
 		System.out.print("After removing duplicates : "+Arrays.toString(arr2));
