3f0ea88f40e79ccd063a4ab4614353df03207091
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-07 23:41:34 +0530

    Implement word count feature in WordsCount.java
    Added functionality to count words in a string input.

--- Strings/WordsCount.java
+++ Strings/WordsCount.java
@@ -1,4 +1,12 @@
-int count = 0;
+import java.util.*;
+public class WordsCount {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a string : ");
+		String word = sc.nextLine();
+		sc.close();
+		int count = 0;
 		for(int i = 0; i < word.length(); i++) {
 		if(word.charAt(i) != ' ' && (i == 0 || word.charAt(i-1) == ' ')) count++;
 		}
