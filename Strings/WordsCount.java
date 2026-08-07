15dd50902da1cd2cbd3fea2aaf8df60ba963765d
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-07 23:42:22 +0530

    Fix word count logic in WordsCount.java

--- Strings/WordsCount.java
+++ Strings/WordsCount.java
@@ -8,6 +8,9 @@ public class WordsCount {
 		sc.close();
 		int count = 0;
 		for(int i = 0; i < word.length(); i++) {
-		if(word.charAt(i) != ' ' && (i == 0 || word.charAt(i-1) == ' ')) count++;
+			if(word.charAt(i) != ' ' && (i == 0 || word.charAt(i-1) == ' ')) 
+				count++;
 		}
 		System.out.println(count);
+	}
+}
