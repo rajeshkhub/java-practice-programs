b3b7fef61978311217ac425052d68314865165da
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-08 17:15:55 +0530

    Change input prompt and variable for word counting

--- Strings/WordsCount.java
+++ Strings/WordsCount.java
@@ -3,14 +3,14 @@ public class WordsCount {
 	public static void main(String[] args) {
 	
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a string : ");
-		String word = sc.nextLine();
+		System.out.println("Enter a sentence : ");
+		String line = sc.nextLine();
 		sc.close();
 		int count = 0;
-		for(int i = 0; i < word.length(); i++) {
-			if(word.charAt(i) != ' ' && (i == 0 || word.charAt(i-1) == ' ')) 
+		for(int i = 0; i < line.length(); i++) {
+			if(line.charAt(i) != ' ' && (i == 0 || line.charAt(i-1) == ' ')) 
 				count++;
 		}
-		System.out.println(count);
+		System.out.println("Number of words are : "+count);
 	}
 }
