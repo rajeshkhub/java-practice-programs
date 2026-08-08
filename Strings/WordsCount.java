2fcbae5ea5abb7e7613211aebd03ae9db2b8c034
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-08 17:21:52 +0530

    Enhance code clarity with comments in WordsCount.java
    Added comments to clarify the purpose of the code and the logic behind the word counting.

--- Strings/WordsCount.java
+++ Strings/WordsCount.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class WordsCount {
 	public static void main(String[] args) {
-	
+															// Counts number of words in a sentence.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a sentence : ");
-		String line = sc.nextLine();
-		sc.close();
-		int count = 0;
-		for(int i = 0; i < line.length(); i++) {
-			if(line.charAt(i) != ' ' && (i == 0 || line.charAt(i-1) == ' ')) 
-				count++;
+		String line = sc.nextLine();						// Input sentence.
+		sc.close();											// Scanner closed.
+		int count = 0;										// count variable counts number of words.		
+		for(int i = 0; i < line.length(); i++) {			// for loop to iterate over input sentence.
+			if(line.charAt(i) != ' ' && (i == 0 || line.charAt(i-1) == ' ')) 	// Checks if current character is not a space along with if index is at start or previous character is a space.
+				count++;									// If the condition is true count increases.
 		}
 		System.out.println("Number of words are : "+count);
 	}
