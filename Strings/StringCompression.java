a6130ca997effb3127d3d52b7894c3e453e74519
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-09 17:59:12 +0530

    Optimize string compression logic
    Refactor inner loop to simplify character comparison and counting.

--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -5,25 +5,19 @@ public class StringCompression {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a string : ");
 		String word = sc.next();
-		int num = 1;									// Used to store index to start with new group of characters.
 		System.out.print("String after compressing : ");
 		
 		for(int i = 0; i < word.length(); i++) {		// Outer loop starts with 0 index.
 			int count = 1;								// Used to count the number of consecutive repeating characters.
 			
-			for(int j = num; j < word.length(); j++) {	// Inner loop starts with index of num value.
-				if(word.charAt(i) == word.charAt(j)) {	// Comparing characters.
+			for(int j = i+1; j < word.length(); j++) {	// Inner loop starts with index of num value.
+				if(word.charAt(i) == word.charAt(j))	// Comparing characters.
 					count++;							// Count increment if characters are same.
-					num = j+2;							// num gets updated index to start with.
-				}
-				else {
-					num = j+1;
-					break;
-				}
+				else break;
 			}
 			if(count > 1) {
 				System.out.print(word.charAt(i)+""+count);
-				i = num-2;
+				i += count-1;
 			}
 			else System.out.print(word.charAt(i));
 		}
