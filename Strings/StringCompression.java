43bb0a9d8ceeb83fcd4e9ad55bb88a2372fd847a
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-09 18:02:11 +0530

    Refactor StringCompression logic for clarity

--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -13,13 +13,13 @@ public class StringCompression {
 			for(int j = i+1; j < word.length(); j++) {	// Inner loop starts with index of num value.
 				if(word.charAt(i) == word.charAt(j))	// Comparing characters.
 					count++;							// Count increment if characters are same.
-				else break;
+				else break;								// else break inner loop.
 			}
-			if(count > 1) {
-				System.out.print(word.charAt(i)+""+count);
-				i += count-1;
+			if(count > 1) {								// Checks if count greater than one.
+				System.out.print(word.charAt(i)+""+count);		// Prints character and number of consecutive similar characters.
+				i += count-1;							// i gets incremented by count-1.
 			}
-			else System.out.print(word.charAt(i));
+			else System.out.print(word.charAt(i));		// else prints single character.
 		}
     sc.close();
   }
