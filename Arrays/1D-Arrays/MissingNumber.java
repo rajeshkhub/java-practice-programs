cc73231679cd3d70907bfb4ee7591eeb200569ae
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-10 19:16:44 +0530

    Add MissingNumber.java to find missing number
    Implement a program to find the missing number in an array.

---
+++ Arrays/1D-Arrays/MissingNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class MissingNumber {
+	public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+    
+		for(int i = 0; i < size; i++)
+			arr[i] = sc.nextInt();
+      
+		int sum = 0;
+		int num = 1;
+		for(int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			num += (arr.length+1)-i;
+		}
+		System.out.print(num-sum);
