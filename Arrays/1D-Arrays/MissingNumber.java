fe28bcb63fac3cb8cbb48e1380f48b71ef6750a5
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-11 21:24:10 +0530

    Clarify comments regarding missing number calculation
    Updated comments to clarify the range of numbers considered in the missing number calculation.

--- Arrays/1D-Arrays/MissingNumber.java
+++ Arrays/1D-Arrays/MissingNumber.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class MissingNumber {
 	public static void main(String[] args) {
-													// Finds missing number from array which contains Numbers from 1 to N.
+													// Finds missing number from array which contains Numbers from 1 to array length.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter size of the array : ");
 		int size = sc.nextInt();					// Size of the array.
@@ -12,12 +12,12 @@ public class MissingNumber {
 			arr[i] = sc.nextInt();
       
 		int sum = 0;								// sum integer varaible to calculate sum of the array elements.
-		int num = 1;								// Variable num to add numbers from 1 to N.
+		int num = 1;								// Variable num to add numbers from 1 to (1 + array length).
 		for(int i = 0; i < arr.length; i++) {		// for loop to iterate over array.
 			sum += arr[i];							// sum of all array numbers.
-			num += (arr.length+1)-i;				// sum of numbers 1 to N.
+			num += (arr.length+1)-i;				// sum of numbers 1 to (1 + array length).
 		}
-		System.out.print(num-sum);					// Print missing number.
+		System.out.print("Missing number is : "+num-sum);	// Print missing number.
 		sc.close();									// Scanner close.
 	}
 }
