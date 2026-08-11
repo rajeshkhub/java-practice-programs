75bd6a1330f0b8f761c69371aa01d3d7d01eccb4
rajesh <147789852+rajeshkhub@users.noreply.github.com>
2026-08-11 21:19:43 +0530

    Enhance MissingNumber.java with code comments
    Added comments to clarify code functionality and variable purposes.

--- Arrays/1D-Arrays/MissingNumber.java
+++ Arrays/1D-Arrays/MissingNumber.java
@@ -1,21 +1,23 @@
 import java.util.*;
 public class MissingNumber {
 	public static void main(String[] args) {
-
-    Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
+													// Finds missing number from array which contains Numbers from 1 to N.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of the array : ");
+		int size = sc.nextInt();					// Size of the array.
 		int[] arr = new int[size];
+		System.out.println("Enter array numbers skipping any one number from 1 to N : ");
     
-		for(int i = 0; i < size; i++)
+		for(int i = 0; i < size; i++)				// for loop to insert input elements into the array.
 			arr[i] = sc.nextInt();
       
-		int sum = 0;
-		int num = 1;
-		for(int i = 0; i < arr.length; i++) {
-			sum += arr[i];
-			num += (arr.length+1)-i;
+		int sum = 0;								// sum integer varaible to calculate sum of the array elements.
+		int num = 1;								// Variable num to add numbers from 1 to N.
+		for(int i = 0; i < arr.length; i++) {		// for loop to iterate over array.
+			sum += arr[i];							// sum of all array numbers.
+			num += (arr.length+1)-i;				// sum of numbers 1 to N.
 		}
-		System.out.print(num-sum);
-		sc.close();
+		System.out.print(num-sum);					// Print missing number.
+		sc.close();									// Scanner close.
 	}
 }
